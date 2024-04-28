33fedb9b8106f2dcbea70f01114e6719c7478954
wail-uottawa <wgueaieb@uottawa.ca>
2024-04-27 23:11:12 -0400

    Testing the new branch (noetic)

--- docker-run-vm.sh
+++ docker-run-vm.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 sudo docker run -it --rm --privileged --name docker-ros-elg5228 \
-       -p 5901:5901 \
-       -p 6901:6901 \
+       -p 6080:80 \
        -e VNC_RESOLUTION=1920x1080 \
-       --volume ~/course_dir:/home/ros/catkin_ws/src/course_dir \
-       realjsk/docker-ros-elg5228:20210908 \
+       --volume ~/course_dir:/home/ubuntu/catkin_ws/src/course_dir \
+       realjsk/docker-ros-noetic-vnc:20240428 \
        bash
 
